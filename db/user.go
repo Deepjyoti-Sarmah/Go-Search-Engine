8b0a470dc8d57e23d557ce32db9cb34229ed6e5c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-17 13:08:55 +0530

    adding user model with create, login admin and adding response target script for better error handling in htmx

--- db/user.go
+++ db/user.go
@@ -1,6 +1,11 @@
 package db
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
   ID string `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
@@ -17,4 +22,30 @@ func (u *User) CreateAdmin() error {
 	Password: "Your Password",
 	IsAdmin: true,
   }
+
+  // Hash Password
+  password, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+  if err != nil {
+	return errors.New("error creating password")
+  }
+  user.Password = string(password)
+
+  //create user in db
+  if err := DBConn.Create(&user).Error; err != nil {
+	return errors.New("Error creating user")
+  }
+  return nil
+}
+
+func (u *User) LoginAsAdmin(email string, password string) (*User, error) {
+  //Find 
+  if err := DBConn.Where("email = ? AND is_Admin = ?", email, true).First(&u).Error; err != nil {
+	return nil, errors.New("user not found")
+  } 
+
+  //Compare the password
+  if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+	return nil, errors.New("Invalid password")
+  }
+  return u, nil
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -24,5 +26,16 @@ func CreateNewAuthToken(id string, email string, isAdmin bool) (string, error)  
 	},
   }
   // create token
+  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+  secretKey, exists := os.LookupEnv("SECRET_KEY")
+  if !exists {
+	panic("SECRET_KEY cannot be found in .env")
+  }
 
+  signedToken, err := token.SignedString([]byte(secretKey))
+  if err != nil {
+	return "", errors.New("error signing the token")
+  }
+
+  return signedToken, nil
 }
